8c2ad66216db46e7d21dc6410cde9a0b078c5509
jwasilko25 <105300237+jwasilko25@users.noreply.github.com>
2024-01-30 16:28:33 -0500

    added movement script

---
+++ NetworkGameDev/Assets/PlayerMovementScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+
+    private void HandleMovement()
+    {
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
